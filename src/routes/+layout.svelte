567074275529f4ca5435417b42b8eded004848f2
Caio Rodrigues <caiordm08@gmail.com>
2026-01-02 19:32:42 -0300

    feat: adicionando speed insigths do vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
     import { dev } from "$app/environment";
     import { inject } from "@vercel/analytics";
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
     inject({ mode: dev ? "development" : "production" });
-
+    injectSpeedInsights();
     import "../app.css";
 </script>
 
